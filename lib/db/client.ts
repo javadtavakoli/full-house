f846ba873f0d4bbfd5d7cd76a0d592f5a28d9283
Javad Tavakoli <javadtavakoli95@gmail.com>
2026-05-25 18:12:01 +0330

    feat(db): drizzle config + postgres client singleton

---
+++ lib/db/client.ts
@@ -0,0 +1,18 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "./schema";
+import { env } from "@/lib/env";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __dbPool: Pool | undefined;
+}
+
+const pool =
+  global.__dbPool ??
+  new Pool({ connectionString: env.DATABASE_URL, max: 10 });
+
+if (process.env.NODE_ENV !== "production") global.__dbPool = pool;
+
+export const db = drizzle(pool, { schema });
+export type DB = typeof db;
